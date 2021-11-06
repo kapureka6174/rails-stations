b278223f976b919259df4dabd43298e45bd09835
kapureka6174 <mitsu.n.0109@gmail.com>
2021-11-06 10:38:12 +0900

    station2

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get '/movies', to: 'movies#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :admin do
+    get '/movies', to: 'movies#index'
+  end
 end
